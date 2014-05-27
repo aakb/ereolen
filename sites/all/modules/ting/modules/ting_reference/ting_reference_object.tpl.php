064364822c745bb3622ed28515357c4ff7529c76
Jesper Kristensen <cableman@linuxdev.dk>
2014-05-27 13:19:17 +0200

    Added new theme function to ting_ref

---
+++ sites/all/modules/ting/modules/ting_reference/ting_reference_object.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Template to render a preview of Ting references.
+ */
+?>
+<div class="ting_reference">
+   <div class="info">
+    <h3 class="title">
+      <?php print l($object->title, $object->url); ?>
+    </h3>
+  </div>
+</div>
+
